68f661e829f887ea9d2817db08fd57513f81098d
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-01-14 19:06:18 +1100

    Initial working version

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	"github.com/kirillrdy/nadeshiko/html"
+	_ "github.com/lib/pq"
+)
+
+func rootHandle(response http.ResponseWriter, request *http.Request) {
+	page := html.Html().Children(
+		html.Form().Action("/upload").Attribute("enctype", "multipart/form-data").Method("POST").Children(
+			html.Input().Type("file").Name("file"),
+			html.Input().Type("submit"),
+		),
+	)
+	io.WriteString(response, page.String())
+}
+
+func fileUpload(response http.ResponseWriter, request *http.Request) {
+	request.ParseMultipartForm(1)
+	form := request.MultipartForm
+	formFile := form.File["file"]
+	file, err := formFile[0].Open()
+	log.Print(formFile[0].Filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	destinationFile, err := os.Create("file")
+	io.Copy(destinationFile, file)
+	defer file.Close()
+	defer destinationFile.Close()
+	http.Redirect(response, request, "/", http.StatusFound)
+}
+
+type Video struct {
+	Id       uint64
+	filename string
+}
+
+func main() {
+	db, err := gorm.Open("postgres", "dbname=vidos sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&Video{})
+
+	http.HandleFunc("/", rootHandle)
+	http.HandleFunc("/upload", fileUpload)
+	err = http.ListenAndServe(":3001", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
