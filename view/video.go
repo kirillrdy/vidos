c58d1b05c4c5700d47b6774a1c63087ece6bd180
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-01-26 14:05:15 +1100

    Partial for generating single video

---
+++ view/video.go
@@ -0,0 +1,27 @@
+package view
+
+import (
+	"github.com/kirillrdy/nadeshiko/html"
+	"github.com/kirillrdy/vidos/db"
+	"github.com/kirillrdy/vidos/path"
+	"github.com/sparkymat/webdsl/css"
+	"github.com/sparkymat/webdsl/css/size"
+)
+
+const videoThumb css.Class = "video-thumb"
+
+func VideoCss() css.RuleSet {
+	return videoThumb.Style(
+		//css.Width(size.Px(ffmpeg.ThumbnailWidth)),
+		css.Width(size.Px(196)),
+	)
+}
+
+//For a given video returns its view partial
+func Video(video db.Video) html.Node {
+	return html.Div().Class(vbox).Children(
+		html.H1().Text(video.Filename),
+		html.Img().Class(videoThumb).Src(path.ThumbnailPath(video)),
+		html.A().Href(path.ServeVideoPath(video)).Text("View"),
+	)
+}
