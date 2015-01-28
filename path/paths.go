ac48152986c309f08014d6393421e2c48695f912
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-01-28 20:29:32 +1100

    New paths for subtitle handling

--- path/paths.go
+++ path/paths.go
@@ -13,14 +13,17 @@ const Public = "/public/"
 const CssReset = Public + "reset.css"
 
 const Videos = "/videos"
+const ViewVideo = "/view_video"
 const UnencodedVideos = "/unencoded_videos"
 const Upload = "/upload"
+const UploadSubtitle = "/upload_subtitle"
 const Serve = "/serve"
 const Download = "/download"
 const Reencode = "/reencode"
 
 const NewVideo = "/new_video"
 const Thumbnail = "/thumbnail"
+const Subtitle = "/subtitle.vtt"
 
 func ServeVideoPath(video db.Video) string {
 	return fmt.Sprintf("%v?id=%v", Serve, video.Id)
@@ -36,4 +39,16 @@ func ReencodeVideoPath(video db.Video) string {
 
 func ThumbnailPath(video db.Video) string {
 	return fmt.Sprintf("%v?id=%v", Thumbnail, video.Id)
+}
+
+func ViewVideoPath(video db.Video) string {
+	return fmt.Sprintf("%v?id=%v", ViewVideo, video.Id)
+}
+
+func UploadSubtitlePath(video db.Video) string {
+	return fmt.Sprintf("%v?id=%v", UploadSubtitle, video.Id)
+}
+
+func SubtitlePath(subtitle db.Subtitle) string {
+	return fmt.Sprintf("%v?id=%v", Subtitle, subtitle.Id)
 }
