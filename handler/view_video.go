ee402084fd0e8bd61bd5b82c53c880f9ce11c655
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-01-28 20:27:57 +1100

    New view video view thingy with subtitles

---
+++ handler/view_video.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/kirillrdy/nadeshiko/html"
+	"github.com/kirillrdy/vidos/db"
+	"github.com/kirillrdy/vidos/path"
+	"github.com/kirillrdy/vidos/view"
+)
+
+func ViewVideo(response http.ResponseWriter, request *http.Request) {
+	video, err := videoFromRequest(request)
+
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+	}
+
+	var subtitles []db.Subtitle
+	db.Session.Find(&subtitles, db.Subtitle{VideoId: video.Id})
+
+	var videoElementContent []html.Node
+
+	//TODO dont hardwire type
+	videoElementContent = append(videoElementContent, html.Source().Src(path.ServeVideoPath(video)).Type("video/mp4"))
+
+	for _, subtitle := range subtitles {
+		track := html.Track().Label("English").Kind("captions").Srclang("en").Src(path.SubtitlePath(subtitle)).Default()
+		videoElementContent = append(videoElementContent, track)
+	}
+
+	videoElement := html.Video().Controls().Autoplay().Name("media").Children(videoElementContent...)
+
+	inside := html.Div().Children(
+		videoElement,
+		html.Form().Action(path.UploadSubtitlePath(video)).Attribute("enctype", "multipart/form-data").Method("POST").Children(
+			html.Input().Type("file").Multiple().Name(view.FormParamName),
+			html.Input().Type("submit"),
+		),
+	)
+	page := view.Layout(inside)
+	io.WriteString(response, page.String())
+}
