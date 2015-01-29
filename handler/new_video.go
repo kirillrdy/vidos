a505749429a8c625bdd6f21e7ed11f32b790e23e
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-01-29 22:14:59 +1100

    Some text for the form

--- handler/new_video.go
+++ handler/new_video.go
@@ -11,9 +11,12 @@ import (
 
 func NewVideo(response http.ResponseWriter, request *http.Request) {
 	page := view.Layout(
-		html.Form().Action(path.Upload).Attribute("enctype", "multipart/form-data").Method("POST").Children(
-			html.Input().Type("file").Attribute("multiple", "multiple").Name(view.FormParamName),
-			html.Input().Type("submit"),
+		html.Div().Children(
+			html.Span().Text("Select file to upload"),
+			html.Form().Action(path.Upload).Multiple().Method("POST").Children(
+				html.Input().Type("file").Attribute("multiple", "multiple").Name(view.FormParamName),
+				html.Input().Type("submit"),
+			),
 		),
 	)
 	io.WriteString(response, page.String())
