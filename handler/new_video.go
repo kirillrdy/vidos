0f2e057884967715239315b43af3c43a506beff5
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-01-30 07:15:49 +1100

    Regression

--- handler/new_video.go
+++ handler/new_video.go
@@ -13,8 +13,8 @@ func NewVideo(response http.ResponseWriter, request *http.Request) {
 	page := view.Layout(
 		html.Div().Children(
 			html.Span().Text("Select file to upload"),
-			html.Form().Action(path.Upload).Multiple().Method("POST").Children(
-				html.Input().Type("file").Attribute("multiple", "multiple").Name(view.FormParamName),
+			html.Form().Action(path.Upload).Attribute("enctype", "multipart/form-data").Method("POST").Children(
+				html.Input().Type("file").Multiple().Name(view.FormParamName),
 				html.Input().Type("submit"),
 			),
 		),
