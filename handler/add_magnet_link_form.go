7467be513eb3a0b2fcb9a1dd9a6768eb7a8d4826
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-09-24 07:33:12 +1000

    Actually add magnet link !!!

--- handler/add_magnet_link_form.go
+++ handler/add_magnet_link_form.go
@@ -2,8 +2,10 @@ package handler
 
 import (
 	"github.com/kirillrdy/nadeshiko/html"
+	"github.com/kirillrdy/vidos/downloader"
 	"github.com/kirillrdy/vidos/path"
 	"github.com/kirillrdy/vidos/view"
+	"log"
 	"net/http"
 )
 
@@ -12,12 +14,29 @@ const MagnetLink = "magnet_link"
 func AddMagnetLink(response http.ResponseWriter, request *http.Request) {
 	if request.Method == "GET" {
 		magnetLinkForm(response, request)
+	} else {
+		request.ParseForm()
+		log.Print(request.Form.Get(MagnetLink))
+
+		torrentFile, err := downloader.Client.AddMagnet(request.Form.Get(MagnetLink))
+		//torrentFile, err := downloader.Client.AddMagnet("magnet:?xt=urn:btih:ZOCMZQIPFFW7OLLMIC5HUB6BPCSDEOQU")
+
+		//Return errors to http client
+		if err != nil {
+			http.Error(response, err.Error(), http.StatusInternalServerError)
+			log.Println(err)
+		}
+
+		<-torrentFile.GotInfo()
+		torrentFile.DownloadAll()
+		http.Redirect(response, request, path.Torrents, http.StatusFound)
 	}
 }
 
 func magnetLinkForm(response http.ResponseWriter, request *http.Request) {
 	form := html.Form().Action(path.AddMagnetLink).Method("POST").Children(
 		html.Input().Name(MagnetLink),
+		html.Input().Type("submit"),
 	)
 	page := view.Layout("Add Magnet Link", form)
 	page.WriteTo(response)
