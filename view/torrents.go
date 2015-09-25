b56e9ae74f99aadd8a4933cae0ff2dd9cfc7863d
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-09-25 20:19:26 +1000

    More info for torrent

--- view/torrents.go
+++ view/torrents.go
@@ -1,6 +1,7 @@
 package view
 
 import (
+	"fmt"
 	"github.com/anacrolix/torrent"
 	humanise "github.com/dustin/go-humanize"
 	"github.com/kirillrdy/nadeshiko/html"
@@ -30,6 +31,8 @@ func TorrentsTable(torrents []torrent.Torrent) html.Node {
 					html.Th().Text("Name"),
 					html.Th().Text("Bytes Completed"),
 					html.Th().Text("Length"),
+					html.Th().Text("%"),
+					html.Th().Text("Finished"),
 				),
 			),
 
@@ -51,10 +54,13 @@ func torrentTrs(torrents []torrent.Torrent) []html.Node {
 }
 
 func torrentTr(torrent torrent.Torrent) html.Node {
+	percent := 100 * torrent.BytesCompleted() / torrent.Length()
 	return html.Tr().Children(
 		html.Td().Text(torrent.Name()),
 		html.Td().Text(humanise.Bytes(uint64(torrent.BytesCompleted()))),
 		html.Td().Text(humanise.Bytes(uint64(torrent.Length()))),
+		html.Td().Text(fmt.Sprint(percent)),
+		html.Td().Text(fmt.Sprint(torrent.Length() == torrent.BytesCompleted())),
 	)
 
 }
