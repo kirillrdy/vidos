4288f69f136130ba6769f8ebda9cc153b67f2f0f
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-10-12 21:51:55 +1100

    Removed old commented code + timing of start up

--- downloader/client.go
+++ downloader/client.go
@@ -3,6 +3,7 @@ package downloader
 import (
 	"github.com/anacrolix/torrent"
 	"log"
+	"time"
 )
 
 const FileDir = "files/"
@@ -11,13 +12,7 @@ var Client *torrent.Client
 
 func init() {
 
-	//TODO In future use this
-	//user, err := user.Current()
-	//if err != nil {
-	//	log.Panic(err)
-	//}
-
-	//config := torrent.Config{DataDir: user.HomeDir}
+	now := time.Now()
 	config := torrent.Config{DataDir: FileDir}
 	var err error
 	Client, err = torrent.NewClient(&config)
@@ -30,25 +25,8 @@ func init() {
 	//TODO implement proper shutdown, and call Close as part of it
 	//defer Client.Close()
 
-	//torrentFile, err := Client.AddMagnet("magnet:?xt=urn:btih:ZOCMZQIPFFW7OLLMIC5HUB6BPCSDEOQU")
-
-	////Return errors to http client
-	//if err != nil {
-	//	log.Println(err)
-	//}
-
-	//<-torrentFile.GotInfo()
-	//torrentFile.DownloadAll()
-
-	//go func() {
-	//	for {
-	//		c.WriteStatus(os.Stdout)
-	//		time.Sleep(1 * time.Second)
-	//	}
-	//}()
-
 	//TODO also possibly part of Client.Close()
 	//Client.WaitAll()
-	log.Print("torrent downloader init finished")
+	log.Printf("torrent downloader init finished, took %v", time.Since(now))
 
 }
