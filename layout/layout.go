edb2b66b7ba723b6c1d6ac9002c8c75bf7b390aa
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-10-12 21:52:15 +1100

    New package to extract css related to layout

---
+++ layout/layout.go
@@ -0,0 +1,49 @@
+package layout
+
+import (
+	"github.com/kirillrdy/nadeshiko/html"
+	"github.com/sparkymat/webdsl/css"
+	"github.com/sparkymat/webdsl/css/display"
+	"github.com/sparkymat/webdsl/css/flex"
+	"github.com/sparkymat/webdsl/css/size"
+)
+
+const VBox css.Class = "vbox"
+const HBox css.Class = "hbox"
+const Wrap css.Class = "wrap"
+const Grow css.Class = "grow"
+const NoGrow css.Class = "no-grow"
+
+func Styles() html.Node {
+	style := css.Stylesheet(
+		css.AllSelectors(css.Body, css.Html).Style(
+			css.Width(size.Percent(100)),
+			css.Height(size.Percent(100)),
+		),
+		VBox.Style(
+			css.Display(display.Flex),
+			css.FlexDirection(flex.Column),
+		),
+		HBox.Style(
+			css.Display(display.Flex),
+			css.FlexDirection(flex.Row),
+		),
+		Wrap.Style(
+			css.FlexWrap(flex.Wrap),
+			//TODO technically this doesn't belong here, but i think it will only ever be used here
+			css.AlignContent(css.FlexStart),
+		),
+		Grow.Style(
+			css.FlexGrow(1),
+		),
+		NoGrow.Style(
+			css.FlexShrink(0),
+		),
+		//centerItems.Style(
+		//	css.AlignItems(css.Center),
+		//),
+	)
+	return html.Style().Text(
+		style.String(),
+	)
+}
