b0f0e7fd49348a8588f5a06c933453fc487cf3a6
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-10-19 09:33:14 +1100

    DRYed file upload

--- handler/upload_file.go
+++ handler/upload_file.go
@@ -4,13 +4,11 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"mime/multipart"
 	"net/http"
 	"os"
 
 	"github.com/kirillrdy/vidos/downloader"
 	"github.com/kirillrdy/vidos/path"
-	"github.com/kirillrdy/vidos/view"
 )
 
 //UploadFile handler accepts file being posted
@@ -21,18 +19,25 @@ func UploadFile(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	//TODO fix assumption on buffer size
-	err := request.ParseMultipartForm(1024 * 1024)
-	if err != nil {
-		http.Error(response, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	form := request.MultipartForm
-	formFiles := form.File[view.FileParamName]
+	handleMultiFileUpload(response, request, func(file io.ReadCloser, fileName string) {
+		defer file.Close()
 
-	for _, formFile := range formFiles {
-		processFormFile(formFile)
-	}
+		uploadedFile := uploadedFile{Filename: fileName}
+
+		//TODO what if Path() didn't exist
+		destinationFile, err := os.Create(uploadedFile.Path())
+		defer destinationFile.Close()
+
+		//TODO Less Fatal
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		n, err := io.Copy(destinationFile, file)
+		if n == 0 || err != nil {
+			log.Fatal(err)
+		}
+	})
 
 	http.Redirect(response, request, path.Files.List, http.StatusFound)
 }
@@ -44,33 +49,4 @@ type uploadedFile struct {
 func (file uploadedFile) Path() string {
 	//TODO use path seperator
 	return fmt.Sprintf("%v/%v", downloader.FileDir, file.Filename)
-}
-
-func processFormFile(formFile *multipart.FileHeader) {
-
-	//TODO does this needs to be closed ?
-	file, err := formFile.Open()
-	defer file.Close()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Printf("Received %#v", formFile.Filename)
-
-	uploadedFile := uploadedFile{Filename: formFile.Filename}
-
-	//TODO what if Path() didn't exist
-	destinationFile, err := os.Create(uploadedFile.Path())
-	defer destinationFile.Close()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	n, err := io.Copy(destinationFile, file)
-	if n == 0 || err != nil {
-		log.Fatal(err)
-	}
-
 }
