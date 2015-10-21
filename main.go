b73f2e2a95237c454cd5e9df56ea36083b8e2ba6
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-10-22 09:29:56 +1100

    Moved a lot of stuff out of the main

--- main.go
+++ main.go
@@ -5,67 +5,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/kirillrdy/vidos/db"
 	"github.com/kirillrdy/vidos/ffmpeg"
-	"github.com/kirillrdy/vidos/handler"
-	"github.com/kirillrdy/vidos/path"
+	"github.com/kirillrdy/vidos/routes"
 )
-
-//TODO move to own package
-func logTimeMiddleware(handler http.HandlerFunc) http.HandlerFunc {
-	return func(response http.ResponseWriter, request *http.Request) {
-		start := time.Now()
-		handler(response, request)
-		log.Printf("%v %v took %v", request.Method, request.URL.Path, time.Since(start))
-	}
-}
-
-func addHandlers() {
-	//TODO Getting more routes move somewhere
-
-	//Files related routes
-	http.HandleFunc(path.UploadFile, handler.UploadFile)
-	http.HandleFunc(path.Files.List, handler.Files)
-	http.HandleFunc(path.DeleteFileOrDirectory, handler.DeleteFileOrDirectory)
-	http.HandleFunc(path.AddFileForEncoding, handler.AddFileForEncoding)
-
-	//Videos
-	http.HandleFunc(path.Videos.List, handler.Videos.List)
-	http.HandleFunc(path.Videos.Show, handler.Videos.Show)
-	http.HandleFunc(path.Videos.Stream, logTimeMiddleware(handler.Stream))
-	http.HandleFunc(path.Videos.Download, handler.Download)
-	http.HandleFunc(path.Videos.Delete, handler.DeleteVideo)
-	http.HandleFunc(path.Videos.New, handler.NewVideo)
-	http.HandleFunc(path.Videos.Thumbnail, handler.Thumbnail)
-
-	//Subtitles
-	http.HandleFunc(path.ManageSubtitles, handler.ManageSubtitles)
-	http.HandleFunc(path.Subtitle, handler.Subtitle)
-	http.HandleFunc(path.UploadSubtitle, handler.SubtitlesUpload)
-
-	http.HandleFunc(path.Videos.Unencoded, handler.UnencodedVideos)
-	http.HandleFunc(path.Videos.Create, handler.VideosUpload)
-
-	http.HandleFunc(path.Torrents, handler.Torrents)
-	http.HandleFunc(path.TorrentStatus, handler.TorrentStatus)
-	http.HandleFunc(path.AddMagnetLink, handler.AddMagnetLink)
-
-	http.HandleFunc(path.Root, handler.RootHandle)
-	http.Handle(path.Public, http.StripPrefix(path.Public, http.FileServer(http.Dir("public"))))
-
-}
 
 func main() {
 
 	ffmpeg.CheckVersion()
+	//TODO sort out how this is currently non blocking using buffered chan
 	db.QueueAllUnEncodedVideos()
 
-	//TODO move flags somewhere
 	port := flag.Int("port", 3001, "Port to listen on")
 
-	addHandlers()
+	routes.AddHandlers()
 
 	log.Printf("Listening on port: '%v'", *port)
 	err := http.ListenAndServe(fmt.Sprintf(":%v", *port), nil)
---
+++ routes/routes.go
@@ -0,0 +1,57 @@
+package routes
+
+import (
+	"github.com/kirillrdy/vidos/handler"
+	"github.com/kirillrdy/vidos/path"
+	"log"
+	"net/http"
+	"time"
+)
+
+func addHandler(path string, handler http.HandlerFunc) {
+	http.HandleFunc(path, logTimeMiddleware(handler))
+}
+
+//AddHandlers registers all the handlers with all of our middleware
+func AddHandlers() {
+
+	//Files related routes
+	addHandler(path.UploadFile, handler.UploadFile)
+	addHandler(path.Files.List, handler.Files)
+	addHandler(path.DeleteFileOrDirectory, handler.DeleteFileOrDirectory)
+	addHandler(path.AddFileForEncoding, handler.AddFileForEncoding)
+
+	//Videos
+	addHandler(path.Videos.List, handler.Videos.List)
+	addHandler(path.Videos.Show, handler.Videos.Show)
+	addHandler(path.Videos.Stream, handler.Stream)
+	addHandler(path.Videos.Download, handler.Download)
+	addHandler(path.Videos.Delete, handler.DeleteVideo)
+	addHandler(path.Videos.New, handler.NewVideo)
+	addHandler(path.Videos.Thumbnail, handler.Thumbnail)
+
+	//Subtitles
+	addHandler(path.ManageSubtitles, handler.ManageSubtitles)
+	addHandler(path.Subtitle, handler.Subtitle)
+	addHandler(path.UploadSubtitle, handler.SubtitlesUpload)
+
+	addHandler(path.Videos.Unencoded, handler.UnencodedVideos)
+	addHandler(path.Videos.Create, handler.VideosUpload)
+
+	addHandler(path.Torrents, handler.Torrents)
+	addHandler(path.TorrentStatus, handler.TorrentStatus)
+	addHandler(path.AddMagnetLink, handler.AddMagnetLink)
+
+	addHandler(path.Root, handler.RootHandle)
+	http.Handle(path.Public, http.StripPrefix(path.Public, http.FileServer(http.Dir("public"))))
+
+}
+
+//TODO perhaps wrap all handlers with this middleware
+func logTimeMiddleware(handler http.HandlerFunc) http.HandlerFunc {
+	return func(response http.ResponseWriter, request *http.Request) {
+		start := time.Now()
+		handler(response, request)
+		log.Printf("%v %v took %v", request.Method, request.URL.Path, time.Since(start))
+	}
+}
