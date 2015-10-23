7c0f8f515124c8cc54dbb50b5333aa0b22b66fd7
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-10-23 15:12:06 +1100

    Some more comments, just so that we do things better

--- downloader/client.go
+++ downloader/client.go
@@ -5,12 +5,16 @@ import (
 	"log"
 )
 
+//FilesDir is where all complete files are stored,
+// not to be confused with videos/
 const FileDir = "files/"
 
+//Client is a pointer to
 var Client *torrent.Client
 
 func init() {
 
+	//TODO torrent should download to own dir and move files once completed
 	config := torrent.Config{DataDir: FileDir}
 	var err error
 	Client, err = torrent.NewClient(&config)
