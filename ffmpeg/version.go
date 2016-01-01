debd7264b2162f931dcc2b220689b041ef99d5cc
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2016-01-01 12:38:27 +0900

    Don't crash when ffmpeg is not found

--- ffmpeg/version.go
+++ ffmpeg/version.go
@@ -1,6 +1,7 @@
 package ffmpeg
 
 import (
+	"errors"
 	"log"
 	"os/exec"
 	"regexp"
@@ -11,10 +12,10 @@ import (
 var testedVersions = []string{"2.3.6", "2.6.4", "2.6.5", "2.8.1", "2.8.3"}
 
 //Version returns the version of ffmpeg found in path
-func Version() string {
+func Version() (string, error) {
 	out, err := exec.Command("ffmpeg", "-version").Output()
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
 	versionRegex := regexp.MustCompile("version (.*?) Copyright")
@@ -22,23 +23,33 @@ func Version() string {
 
 	//Failed to match
 	if len(result) != 2 {
-		return ""
+		//TODO print output for better debugging
+		return "", errors.New("Failed to get version from output of ffmpeg -version. report this to maintainer")
 	}
-	return result[1]
+	return result[1], nil
 }
 
 //CheckVersion checks that system ffmpeg is of the same version as this lib was built against
 //No errors raised just warning is printed
 func CheckVersion() {
+
+	ffmpegVersion, err := Version()
+
+	if err != nil {
+		log.Printf("WARNING: Failed to detect version of ffmpeg: %v", err.Error())
+		return
+	}
+
 	var foundVersion bool
 	for _, testedVersion := range testedVersions {
-		if Version() == testedVersion {
+		if ffmpegVersion == testedVersion {
 			foundVersion = true
 		}
 	}
+
 	if foundVersion == false {
 		log.Print("WARNING: running against untested version of ffmpeg")
 		log.Printf("WARNING: recommended versions are %#v", strings.Join(testedVersions, ", "))
-		log.Printf("WARNING: detected version is %#v", Version())
+		log.Printf("WARNING: detected version is %#v", ffmpegVersion)
 	}
 }
