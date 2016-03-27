e6b6cb73601067cfbf17fc1635b02eb5a7e4472e
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2016-03-27 11:26:59 +0900

    Display no videos message

--- view/videos.go
+++ view/videos.go
@@ -15,12 +15,16 @@ const FileParamName = "file"
 //Videos returns a page listing videos in a growing wrapped flexbox
 func Videos(videos []fs.Video) html.Node {
 
-	var divs []html.Node
+	divs := make([]html.Node, 0, len(videos))
 
 	divs = append(divs, html.Style().Text(VideoCSS().String()))
 
 	for _, video := range videos {
 		divs = append(divs, Video(video))
+	}
+
+	if len(videos) == 0 {
+		divs = append(divs, html.Span().Text("No videos"))
 	}
 
 	page := html.Div().Class(layout.HBox, layout.Grow, layout.Wrap).Children(
