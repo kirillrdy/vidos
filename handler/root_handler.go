88aa82810660cedbd91eac53e8297b212ec38d5f
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2017-04-15 08:58:28 +1000

    Linter

--- handler/root_handler.go
+++ handler/root_handler.go
@@ -6,6 +6,7 @@ import (
 	"github.com/kirillrdy/vidos/path"
 )
 
+// RootHandle redirects to default route
 func RootHandle(response http.ResponseWriter, request *http.Request) {
 	http.Redirect(response, request, path.Videos.List, http.StatusFound)
 }
