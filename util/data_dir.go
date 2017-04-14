c1622f5b3418a569b4193143d0a04c7cb17d7dce
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2017-04-15 08:59:03 +1000

    More linting and error handling

--- util/data_dir.go
+++ util/data_dir.go
@@ -1,38 +1,30 @@
 package util
 
 import (
-	"log"
 	"os"
 	"os/user"
 )
 
-//VidosDataDir is where all vidos files will be stored
+// VidosDataDir is where all vidos files will be stored
 //its being set by init() of this package
 var VidosDataDir string
 
-//VidosDataDirFor returns a dir where each submodule can store its data
+// VidosDataDirFor returns a dir where each submodule can store its data
 //It also creates a directory if it didn't exist
 func VidosDataDirFor(suffix string) string {
 
 	dir := VidosDataDir + string(os.PathSeparator) + suffix
-
 	err := os.MkdirAll(dir, os.ModePerm)
-	if err != nil {
-		log.Panic(err)
-	}
+	CrashOnErrors(err)
 	return dir
 }
 
 func init() {
 
 	user, err := user.Current()
-	if err != nil {
-		log.Panic(err)
-	}
+	CrashOnErrors(err)
 
 	VidosDataDir = user.HomeDir + "/.vidos"
 	err = os.MkdirAll(VidosDataDir, os.ModePerm)
-	if err != nil {
-		log.Panic(err)
-	}
+	CrashOnErrors(err)
 }
