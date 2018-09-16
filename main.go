63484d53971dd3955b4b3fd09ec9da26f124dcbc
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2018-09-17 08:46:55 +1000

    I dont know why i ever had this

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	port := flag.Int("port", 3000, "Port to listen on")
 	http2Mode := flag.Bool("http2", false, "Use http2")
-	log.Printf("Listening on port: '%v'", *port)
+	log.Printf("Listening on port: %v", *port)
 
 	address := fmt.Sprintf(":%v", *port)
 
