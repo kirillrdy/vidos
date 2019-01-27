e8c7652c7f850553ebcac1038689b4ed0f0e81be
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2019-01-27 11:22:08 +1100

    Old main retains old behaviour

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/kirillrdy/vidos/downloader"
 	"github.com/kirillrdy/vidos/encoder"
 	"github.com/kirillrdy/vidos/ffmpeg"
 	"github.com/kirillrdy/vidos/handler"
@@ -13,6 +14,7 @@ import (
 func main() {
 	ffmpeg.CheckVersion()
 	encoder.Start()
+	downloader.InitClientAndDownloadMover()
 	handler.AddHandlers()
 
 	port := flag.Int("port", 3000, "Port to listen on")
